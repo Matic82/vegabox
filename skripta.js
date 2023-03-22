68cdbf4184f0d5d0312c09f510162b87c36a9b60
Matic82 <mattilauzana@gmail.com>
2023-03-22 13:40:10 +0100

    Dopolnil skripta.js

--- skripta.js
+++ skripta.js
@@ -1,12 +1,12 @@
 // 1. Dodaj EventListener za nalozitev spletne strani tipa 'load'
-window.addEventListener('/* tip dogodka */', function() {
+window.addEventListener("load", function() {
     // stran nalozena
 
     var prizgiCakanje = function() {
-        document.querySelector('.loading').style.display = // dodajte vrednost za PRIKAZ elementa
+        document.querySelector('.loading').style.display = 'block';
     }
     var ugasniCakanje = function() {
-        document.querySelector('.loading').style.display = // dodajte vrednost za SKRITJE elementa
+        document.querySelector('.loading').style.display = 'none';
     }
 
     // Dodaj 'click' EventListener elementu z id="nalozi"
@@ -28,7 +28,7 @@ window.addEventListener('/* tip dogodka */', function() {
                  *  3. definiraj spremenljivko za enoto (B kot Byte)
                  *  4. sestavi html div ki predstavlja posamezno datoteko
                  */
-                var datoteka = /* Koda gre sem */;
+                var datoteka = /* Koda gre sem ;*/
 
                 var velikost = // Koda gre sem */;
 
@@ -49,4 +49,32 @@ window.addEventListener('/* tip dogodka */', function() {
             // Klici funkcijo ugasniCakanje()
         }
     };
-})
+    var brisi = function(event) {
+        // 1. Klici funkcijo prizgiCakanje()
+        // Koda gre sem... 
+
+        var xhttp = new XMLHttpRequest();
+
+        xhttp.onreadystatechange = function() {
+			if (xhttp.readyState == 4 && xhttp.status == 200) {
+
+                /** 2. Preveri da lastnost responseText objekta xhttp odgovarja "Datoteka izbrisana",
+                 *  v pozitivnem primeru preusmeri uporabnika na glavno stran (pomoč: window.location)
+                 *  sicer sproži alert s sporočilom, ki sporoči uporabniku, da ni moč zbrisati datoteko
+                 */
+				if (xhttp.responseText == "Datoteka izbrisana") {
+					window.location = /* Koda gre sem... */
+				} else {
+					// Koda gre sem...
+				}
+                // 3. Klici funkcijo ugasniCakanje()
+                // Koda gre sem...
+			}
+			
+		};
+        // 4. Poglej sintakso funkcije .open() ter podaj zahtevo strežniku tipa GET
+		xhttp.open("/* Koda gre sem */", "/brisi/"+this.getAttribute("datoteka"), true);
+		// 5. Pošlji zahtevo strežniku s funkcijo send() na objektu xhttp
+        // Koda gre sem...
+    }
+});
